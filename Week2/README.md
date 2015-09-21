1ce5dbea5eee1d287566afa8a06584751fae01bb
not-much-io <kristo.koert@gmail.com>
2015-09-21 23:51:04 +0300

    Added TriangleExample.

--- Week2/README.md
+++ Week2/README.md
@@ -1,7 +1,10 @@
 # Week 2
 
-Let's try to write some actual classes. Included here is the calculator exercise we did in the lecture. This was not a good 
-example program because it was too difficult. However it is there and it is hopefully well documented.
+Let's try to write some actual classes. 
+
+Included here is the calculator exercise we did in the lecture. This was not a good example program because it was unnecessarily complicated. However it is there and it is hopefully well documented.
+
+Also I've included an example Triangle class that might be helpful as reference for solving the below exercises.
 
 The actual exercises after this lecture will be from here:
 
---
+++ Week2/TriangleExample/src/TriangleExample.java
@@ -0,0 +1,52 @@
+
+/*
+* Make an equilateral triangle class
+*
+* The constructor of the class takes the lengths of the sides
+* All fields in the class are private
+* Implement the following methods:
+*
+*   calculateArea
+*   calculatePerimeter
+*   equals
+*
+* */
+
+class EquilateralTriangle {
+
+    private double sideLengths;
+
+    EquilateralTriangle(double sideLengths) {
+        this.sideLengths = sideLengths;
+    }
+
+    double calculateArea() {
+        return (Math.sqrt(3) / 4) * Math.pow(this.sideLengths, 2);
+    }
+
+    double calculatePerimeter() {
+        return sideLengths*3;
+    }
+
+    public boolean compareToOther(EquilateralTriangle other) {
+        return this.calculatePerimeter() == other.calculatePerimeter();
+    }
+
+}
+
+public class TriangleExample {
+
+    public static void main(String[] args) {
+
+        EquilateralTriangle eqlTriangle = new EquilateralTriangle(1.5);
+
+        System.out.println(eqlTriangle.calculateArea());
+
+        System.out.println(eqlTriangle.calculatePerimeter());
+
+        System.out.println(eqlTriangle.compareToOther(new EquilateralTriangle(1.5)));
+
+        System.out.println(eqlTriangle.compareToOther(new EquilateralTriangle(2.0)));
+
+    }
+}
